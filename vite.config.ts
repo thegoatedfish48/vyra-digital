ae9af2b49864f84af55cd726757045c8f7d17a54
thegoatedfish48 <thegoatedfish48@gmail.com>
2026-09-19 19:41:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,14 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
     server: { entry: "server" },
+
+    prerender: {
+      enabled: true,
+      crawlLinks: true,
+      autoStaticPathsDiscovery: true,
+      autoSubfolderIndex: true,
+      failOnError: true,
+    },
   },
 });
